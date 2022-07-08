ad23fa2a856180958718423d47959adc1ff24a52
Farhan Rashid <far20shid@gmail.com>
2022-07-09 03:24:38 +0600

    ININT

---
+++ Assets/Project/Scripts/AnimatorController/AnimatorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    // Start is called before the first frame update
+   public void StateIdle()
+    {
+        animator.Play("IdleState");
+    }
+    public void StateRun()
+    {
+        animator.Play("Run");
+
+
+    }
+    public void StateCarry()
+    {
+
+        animator.Play("Carry");
+
+    }
+    public void StateJump()
+    {
+        animator.Play("Jump");
+
+
+    }
+}
---
+++ Assets/Project/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Touch touch;
+    private Vector2 touchPos;
+   [SerializeField] float rotateSpeed = 5;
+   [SerializeField] float moveSpeed = 5;
+    [SerializeField] AnimatorController animator;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        animator = GetComponent<AnimatorController>();
+    }
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+
+                Quaternion YRot = Quaternion.Euler(0, touch.deltaPosition.x * rotateSpeed * Time.deltaTime, 0);
+
+                transform.rotation = transform.rotation * YRot;
+            }
+
+
+        }
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        animator.StateRun();
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit,10))
+        {
+            Debug.Log(hit.transform.name);
+
+        }
+    }
+}
